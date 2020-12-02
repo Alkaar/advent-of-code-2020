721a8b86b67b4d8807f19dbae6365e810f560f3c
Derek <Alkaar@gmail.com>
2020-12-02 17:23:58 -0500

    QoL changes

--- src/main/scala/Day01.scala
+++ src/main/scala/Day01.scala
@@ -1,6 +1,7 @@
 import scala.io.Source
 
 object Day01 {
+
   def main(args: Array[String]): Unit = {
     println(s"First answer: ${solutionOne}")
     println(s"Second answer: ${solutionTwo}")
@@ -9,16 +10,25 @@ object Day01 {
   def solutionOne: Int = {
     val input = Source.fromResource("day01.txt").getLines.toIndexedSeq
 
-    val numPairs = for(i <- input.indices; j <- i until input.length) yield (input(i).toInt, input(j).toInt)
+    val numPairs = for {
+      i <- input.indices
+      j <- i until input.length
+    } yield (input(i).toInt, input(j).toInt)
 
-    numPairs.foldLeft(0)((res, cur) => if(cur._1 + cur._2 == 2020) cur._1 * cur._2 else res)
+    numPairs.foldLeft(0)((res, cur) => if (cur._1 + cur._2 == 2020) cur._1 * cur._2 else res)
   }
 
   def solutionTwo: Int = {
     val input = Source.fromResource("day01.txt").getLines.toIndexedSeq
 
-    val numPairs = for(i <- input.indices; j <- i until input.length; k <- j until input.length) yield (input(i).toInt, input(j).toInt, input(k).toInt)
+    val numPairs = for {
+      i <- input.indices
+      j <- i until input.length
+      k <- j until input.length
+    } yield (input(i).toInt, input(j).toInt, input(k).toInt)
 
-    numPairs.foldLeft(0)((res, cur) => if(cur._1 + cur._2 + cur._3 == 2020) cur._1 * cur._2 * cur._3 else res)
+    numPairs.foldLeft(0)(
+      (res, cur) => if (cur._1 + cur._2 + cur._3 == 2020) cur._1 * cur._2 * cur._3 else res
+    )
   }
 }
