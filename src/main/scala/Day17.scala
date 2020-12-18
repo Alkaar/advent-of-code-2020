a69d422633c73148d18f7df8b32bee8d7e68ee8b
Derek <Alkaar@gmail.com>
2020-12-17 22:22:13 -0500

    Day 17 solution solved with types

--- src/main/scala/Day17.scala
+++ src/main/scala/Day17.scala
@@ -1,7 +1,7 @@
-import cats.implicits._
-
 import scala.annotation.tailrec
 import scala.io.Source
+import scala.reflect.{classTag, ClassTag}
+import scala.reflect.runtime.universe._
 
 object Day17 {
 
@@ -10,130 +10,105 @@ object Day17 {
     println(s"Second answer: ${solutionTwo}")
   }
 
-  def solutionOne: Int = {
-    final case class Cube(y: Int, x: Int, z: Int)
+  def initConwayCube[T <: Product with Serializable: TypeTag: ClassTag](): Set[T] = {
+    Source
+      .fromResource("day17.txt")
+      .getLines
+      .foldLeft(Set.empty[T], 0) {
+        case ((conwayCube, rowIndex), row) =>
+          val newConwayCube = row
+            .split("")
+            .zipWithIndex
+            .collect[T] {
+              case (cubeState, colIndex) if cubeState == "#" =>
+                val numExtraAxes = typeTag[T].tpe.decls.collect {
+                  case meth: MethodSymbol if meth.isCaseAccessor => true
+                }.size
 
-    def initConwayCube(): Set[Cube] = {
-      Source
-        .fromResource("day17.txt")
-        .getLines
-        .foldLeft(Set.empty[Cube], 0) {
-          case ((conwayCube, rowIndex), row) =>
-            val newConwayCube = row
-              .split("")
-              .zipWithIndex
-              .collect {
-                case (cubeState, colIndex) if cubeState == "#" => Cube(rowIndex, colIndex, 0)
-              }
-              .toSet
-            (conwayCube ++ newConwayCube, rowIndex + 1)
-        }
-        ._1
-    }
-
-    def playCycle(conwayCube: Set[Cube]): Set[Cube] = {
-      conwayCube
-        .foldLeft(Set.empty[Cube], Set.empty[Cube]) {
-          case ((newConwayCube, visited), cube) =>
-            val cubeNeighbors = generateNeighbors(cube)
-            val partialConwayCube = cubeNeighbors
-              .filterNot(visited.contains)
-              .map(isCubeActive(conwayCube, _, false))
-            (
-              newConwayCube ++ (partialConwayCube :+ isCubeActive(conwayCube, cube, true)).flatten,
-              visited ++ (cubeNeighbors :+ cube)
-            )
-        }
-        ._1
-    }
-
-    def generateNeighbors(cube: Cube): List[Cube] = {
-      (for {
-        y <- cube.y - 1 to cube.y + 1
-        x <- cube.x - 1 to cube.x + 1
-        z <- cube.z - 1 to cube.z + 1
-      } yield Cube(y, x, z)).diff(List(cube)).toList
-    }
-
-    def isCubeActive(conwayCube: Set[Cube], cube: Cube, isActive: Boolean): Option[Cube] = {
-      val numActive = generateNeighbors(cube).count(conwayCube.contains)
-
-      if (numActive == 3 || (isActive && 2 == numActive)) Some(cube) else None
-    }
-
-    @tailrec
-    def playCycles(numCycles: Int, conwayCube: Set[Cube] = initConwayCube()): Set[Cube] = {
-      if (numCycles == 0) conwayCube else playCycles(numCycles - 1, playCycle(conwayCube))
-    }
-
-    playCycles(6).size
+                classTag[T].runtimeClass.getConstructors.head
+                  .newInstance(List(rowIndex, colIndex).padTo(numExtraAxes, 0): _*)
+                  .asInstanceOf[T]
+            }
+            .toSet
+          (conwayCube ++ newConwayCube, rowIndex + 1)
+      }
+      ._1
   }
 
-  def solutionTwo: Long = {
-    final case class HyperCube(y: Int, x: Int, z: Int, w: Int)
+  def playCycle[T <: Product with Serializable: ClassTag](conwayCube: Set[T]): Set[T] = {
+    conwayCube
+      .foldLeft(Set.empty[T], Set.empty[T]) {
+        case ((newConwayCube, visited), cube) =>
+          val cubeNeighbors = generateNeighbors(cube)
+          val partialConwayCube = cubeNeighbors
+            .filterNot(visited.contains)
+            .map(isCubeActive(conwayCube, _, false))
+          (
+            newConwayCube ++ (partialConwayCube :+ isCubeActive(conwayCube, cube, true)).flatten,
+            visited ++ (cubeNeighbors :+ cube)
+          )
+      }
+      ._1
+  }
 
-    def initConwayCube(): Set[HyperCube] = {
-      Source
-        .fromResource("day17.txt")
-        .getLines
-        .foldLeft(Set.empty[HyperCube], 0) {
-          case ((conwayCube, rowIndex), row) =>
-            val newConwayCube = row
-              .split("")
-              .zipWithIndex
-              .collect {
-                case (cubeState, colIndex) if cubeState == "#" =>
-                  HyperCube(rowIndex, colIndex, 0, 0)
-              }
-              .toSet
-            (conwayCube ++ newConwayCube, rowIndex + 1)
-        }
-        ._1
-    }
-
-    def playCycle(conwayCube: Set[HyperCube]): Set[HyperCube] = {
-      conwayCube
-        .foldLeft(Set.empty[HyperCube], Set.empty[HyperCube]) {
-          case ((newConwayCube, visited), cube) =>
-            val cubeNeighbors = generateNeighbors(cube)
-            val partialConwayCube = cubeNeighbors
-              .filterNot(visited.contains)
-              .map(isCubeActive(conwayCube, _, false))
-            (
-              newConwayCube ++ (partialConwayCube :+ isCubeActive(conwayCube, cube, true)).flatten,
-              visited ++ (cubeNeighbors :+ cube)
-            )
-        }
-        ._1
-    }
-
-    def generateNeighbors(cube: HyperCube): List[HyperCube] = {
-      (for {
-        y <- cube.y - 1 to cube.y + 1
-        x <- cube.x - 1 to cube.x + 1
-        z <- cube.z - 1 to cube.z + 1
-        w <- cube.w - 1 to cube.w + 1
-      } yield HyperCube(y, x, z, w)).diff(List(cube)).toList
-    }
-
-    def isCubeActive(
-      conwayCube: Set[HyperCube],
-      cube: HyperCube,
-      isActive: Boolean
-    ): Option[HyperCube] = {
-      val numActive = generateNeighbors(cube).count(conwayCube.contains)
-
-      if (numActive == 3 || (isActive && 2 == numActive)) Some(cube) else None
-    }
-
+  def generateNeighbors[T <: Product with Serializable: ClassTag](cube: T): List[T] = {
     @tailrec
-    def playCycles(
-      numCycles: Int,
-      conwayCube: Set[HyperCube] = initConwayCube()
-    ): Set[HyperCube] = {
-      if (numCycles == 0) conwayCube else playCycles(numCycles - 1, playCycle(conwayCube))
+    def generateNeighborsInner[T <: Product with Serializable: ClassTag](
+      cubeAxes: List[Int],
+      accNeighborCubes: List[List[Int]],
+      index: Int = 0
+    ): List[T] = {
+      if (index == cubeAxes.length) {
+        accNeighborCubes.map(
+          classTag[T].runtimeClass.getConstructors.head
+            .newInstance(_: _*)
+            .asInstanceOf[T]
+        )
+      } else {
+        val newAcc = for {
+          i <- accNeighborCubes
+          j <- List(cubeAxes(index) - 1, cubeAxes(index), cubeAxes(index) + 1)
+        } yield i :+ j
+
+        generateNeighborsInner[T](cubeAxes, newAcc, index + 1)
+      }
     }
 
-    playCycles(6).size
+    val cubeAxes = cube.productIterator.toList.asInstanceOf[List[Int]]
+
+    generateNeighborsInner(
+      cubeAxes.drop(1),
+      List(List(cubeAxes.head - 1), List(cubeAxes.head), List(cubeAxes.head + 1))
+    ).diff(List(cube))
+  }
+
+  def isCubeActive[T <: Product with Serializable: ClassTag](
+    conwayCube: Set[T],
+    cube: T,
+    isActive: Boolean
+  ): Option[T] = {
+    val numActive = generateNeighbors(cube).count(conwayCube.contains)
+
+    if (numActive == 3 || (isActive && 2 == numActive)) Some(cube) else None
+  }
+
+  @tailrec
+  def playCycles[T <: Product with Serializable: ClassTag](
+    conwayCube: Set[T],
+    numCycles: Int
+  ): Set[T] = {
+    if (numCycles == 0) conwayCube else playCycles(playCycle(conwayCube), numCycles - 1)
+  }
+
+  final case class Cube(y: Int, x: Int, z: Int)
+
+  def solutionOne: Int = {
+    playCycles[Cube](initConwayCube[Cube](), 6).size
+  }
+
+  final case class HyperCube(y: Int, x: Int, z: Int, w: Int)
+
+  def solutionTwo: Int = {
+    playCycles[HyperCube](initConwayCube[HyperCube](), 6).size
   }
 }
